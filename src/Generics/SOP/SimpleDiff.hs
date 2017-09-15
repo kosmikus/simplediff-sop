93c4767661f6e1ba7354b57e6f6131f8ef97ce42
Andres Loeh <andres@well-typed.com>
2017-09-15 23:11:04 +0200

    Refactor a bit.

--- src/Generics/SOP/SimpleDiff.hs
+++ src/Generics/SOP/SimpleDiff.hs
@@ -18,58 +18,62 @@ import Generics.SOP
 import qualified GHC.Generics as GHC
 import System.Console.ANSI
 
-data Diff a where
-  Unchanged :: a -> Diff a
-  Changed   :: a -> a -> Diff a
-  Enter     :: SOP Diff (Code a) -> Diff a
+type EqDiff a = a
+type GDiff a = SOP WDiff (Code a)
 
-deriving instance
-  (Show a, All (Compose Show (NP Diff)) (Code a)) => Show (Diff a)
+data WDiff a =
+    Changed a a
+  | Enter (Diff a)
 
 class (Show a, Eq a) => Diffable a where
-  diff :: a -> a -> Diff a
+  type Diff a
+  type Diff a = GDiff a
+  diff :: a -> a -> WDiff a
   default diff ::
-    (Generic a, All2 Diffable (Code a)) => a -> a -> Diff a
+    (Generic a, All2 Diffable (Code a), Diff a ~ GDiff a) => a -> a -> WDiff a
   diff = gDiff
 
-  renderPrec :: Int -> Diff a -> IO ()
+  renderPrec :: Int -> WDiff a -> IO ()
   default renderPrec ::
-    (Generic a, HasDatatypeInfo a, All2 Diffable (Code a))
-    => Int -> Diff a -> IO ()
+    (Generic a, HasDatatypeInfo a, All2 Diffable (Code a), Diff a ~ GDiff a)
+    => Int -> WDiff a -> IO ()
   renderPrec = gRenderPrec
 
-eqDiff :: Eq a => a -> a -> Diff a
+eqDiff :: (Eq a, Diff a ~ EqDiff a) => a -> a -> WDiff a
 eqDiff x y
-  | x == y    = Unchanged x
+  | x == y    = Enter x
   | otherwise = Changed x y
 
 gDiff ::
-  (Generic a, All2 Diffable (Code a)) => a -> a -> Diff a
+  (Generic a, All2 Diffable (Code a), Diff a ~ GDiff a) => a -> a -> WDiff a
 gDiff x y =
   case go (unSOP (from x)) (unSOP (from y)) of
     Nothing -> Changed x y
     Just d  -> Enter (SOP d)
   where
-    go :: All2 Diffable xss => NS (NP I) xss -> NS (NP I) xss -> Maybe (NS (NP Diff) xss)
+    go :: All2 Diffable xss => NS (NP I) xss -> NS (NP I) xss -> Maybe (NS (NP WDiff) xss)
     go (Z xs) (Z ys) = Just (Z (hczipWith (Proxy @Diffable) (coerce diff) xs ys))
     go (S x ) (S y ) = S <$> go x y
     go _      _      = Nothing
 
-showRenderPrec :: (Show a) => Int -> Diff a -> IO ()
-showRenderPrec p (Unchanged a) = putStr (showsPrec p a "")
-showRenderPrec p (Changed x y) = do
+showRenderPrec :: (Show a, Diff a ~ EqDiff a) => Int -> WDiff a -> IO ()
+showRenderPrec p (Enter a) = putStr (showsPrec p a "")
+showRenderPrec p (Changed x y) = renderChange p x y
+
+renderChange :: Show a => Int -> a -> a -> IO ()
+renderChange p x y = do
   setSGR [SetColor Background Vivid Red]
   putStr (showsPrec p x "")
   setSGR [SetColor Background Vivid Green]
   putStr (showsPrec p y "")
   setSGR [Reset]
 
-gRenderPrec :: forall a . (Generic a, Show a, HasDatatypeInfo a, All2 Diffable (Code a)) => Int -> Diff a -> IO ()
+gRenderPrec :: forall a . (Generic a, Show a, HasDatatypeInfo a, All2 Diffable (Code a), Diff a ~ GDiff a) => Int -> WDiff a -> IO ()
 gRenderPrec p (Enter d) = renderSOP (Proxy :: Proxy a) p d
-gRenderPrec p d         = showRenderPrec p d
+gRenderPrec p (Changed x y) = renderChange p x y
 
 renderSOP ::
-  (Generic a, HasDatatypeInfo a, All2 Diffable (Code a)) => Proxy a -> Int -> SOP Diff (Code a) -> IO ()
+  (Generic a, HasDatatypeInfo a, All2 Diffable (Code a)) => Proxy a -> Int -> SOP WDiff (Code a) -> IO ()
 renderSOP p d x =
     hcollapse
   $ hczipWith (Proxy @(All Diffable)) (renderConstructor d)
@@ -81,7 +85,7 @@ renderParen True  x = putStr "(" >> x >> putStr ")"
 renderParen False x = x
 
 renderConstructor ::
-  forall xs . (All Diffable xs) => Int -> ConstructorInfo xs -> NP Diff xs -> K (IO ()) xs
+  forall xs . (All Diffable xs) => Int -> ConstructorInfo xs -> NP WDiff xs -> K (IO ()) xs
 renderConstructor d i =
   case i of
     Constructor n -> \ x -> K
@@ -97,12 +101,12 @@ renderConstructor d i =
       $ putStr (n ++ " {") >> renderRecordArgs fi x >> putStr "}"
 
 renderConstructorArgs ::
-  (All Diffable xs) => Int -> NP Diff xs -> IO ()
+  (All Diffable xs) => Int -> NP WDiff xs -> IO ()
 renderConstructorArgs d x =
   sequence_ $ intersperse (putStr " ") $ hcollapse $ hcmap (Proxy @Diffable) (K . renderPrec d) x
 
 renderRecordArgs ::
-  (All Diffable xs) => NP FieldInfo xs -> NP Diff xs -> IO ()
+  (All Diffable xs) => NP FieldInfo xs -> NP WDiff xs -> IO ()
 renderRecordArgs fi x =
     sequence_
   $ intersperse (putStr ", ")
@@ -112,12 +116,12 @@ renderRecordArgs fi x =
       fi x
 
 renderTuple ::
-  (IsProductType a xs, Show a, All Diffable xs) => Int -> Diff a -> IO ()
-renderTuple _ (Enter d) = renderTuple' (unZ (unSOP d))
-renderTuple p d         = showRenderPrec p d
+  (IsProductType a xs, Show a, All Diffable xs, Diff a ~ GDiff a) => Int -> WDiff a -> IO ()
+renderTuple p (Enter d) = renderTuple' (unZ (unSOP d))
+renderTuple p (Changed x y) = renderChange p x y
 
 renderTuple' ::
-  (All Diffable xs) => NP Diff xs -> IO ()
+  (All Diffable xs) => NP WDiff xs -> IO ()
 renderTuple' x =
     renderParen True
   $ sequence_
@@ -129,22 +133,24 @@ app_prec :: Int
 app_prec = 10
 
 instance Diffable () where
+  type Diff () = EqDiff ()
   diff = eqDiff
   renderPrec = showRenderPrec
 
 instance Diffable Int where
+  type Diff Int = EqDiff Int
   diff = eqDiff
   renderPrec = showRenderPrec
 
 instance Diffable Char where
+  type Diff Char = EqDiff Char
   diff = eqDiff
   renderPrec = showRenderPrec
 
-instance Diffable Bool where
-  diff = eqDiff
-  renderPrec = showRenderPrec
+instance Diffable Bool
 
 instance Diffable Text where
+  type Diff Text = EqDiff Text
   diff = eqDiff
   renderPrec = showRenderPrec
 
@@ -155,6 +161,7 @@ instance (Diffable a, Diffable b) => Diffable (a, b) where
 instance (Diffable a, Diffable b, Diffable c) => Diffable (a, b, c) where
   diff = gDiff
   renderPrec = renderTuple
+
 
 instance Diffable a => Diffable (Maybe a)
 instance (Diffable a, Diffable b) => Diffable (Either a b)
